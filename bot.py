b36864caab001def68d35f3eb633c0cb9fdb934d
rudranshgoel1 <app.whatsupleads@gmail.com>
2026-03-22 00:16:09 +0530

    added the emoji functionality

--- bot.py
+++ bot.py
@@ -77,12 +77,94 @@ def message(payload):
 
     if BOT_ID != user_id:
         ping_match = re.match(r'<@([A-Z0-9]+)>\s+(\S+)$', text.strip())
+        emoji_pet_match = re.match(r':([a-zA-Z0-9])_-]+):\s+(\S+)$', text.strip())
         
         if event.get('bot_id') or event.get('subtype') == 'bot_message':
             return
         
         elif event.get('thread_ts') and event.get('thread_ts') != event.get('ts'):
             return
+        
+        elif emoji_pet_match and not files and not ping_match:
+            source = emoji_pet_match.group(1)
+            new_name = fix_emoji_name(emoji_pet_match.group(2))
+            ts = event.get('ts')
+            
+            existing = client.api_call("emoji.list")
+            all_emoji = existing.get('emoji', {})
+            
+            if new_name in all_emoji:
+                client.chat_postMessage(channel=channel_id, thread_ts=ts, text="emoji with that name already exists :thinkies:")
+                client.chat_postMessage(channel=channel_id, thread_ts=ts, text="uhh send this again with another name")
+                return
+            
+            emoji_url = all_emoji.get(source)
+            
+            if not emoji_url:
+                client.chat_postMessage(channel=channel_id, thread_ts=ts, text=f"couldn't find :{source}: emoji in the workspace brotato chip :loll:")
+                return
+            
+            while emoji_url and emoji_url.startswith('alias:'):
+                alias = emoji_url[len('alias:'):]
+                emoji_url = all_emoji.get(alias)
+                
+            if not emoji_url:
+                client.chat_postMessage(channel=channel_id, thread_ts=ts, text=f"couldn't resolve :{source}: alias :cryin:")
+                return
+            
+            client.reactions_add(channel=channel_id, name='loading', timestamp=ts)
+            client.chat_postMessage(channel=channel_id, thread_ts=ts, text="making emoji...")
+            
+            r = requests.get("https://patpatgifmaker.vercel.app/api/petpet", params={
+                    "image_url": emoji_url,
+                    "slack_token": os.environ["SLACK_TOKEN"],
+                })
+            gif_url = r.json()["gif_url"]
+
+            gif = requests.get(gif_url)
+
+            files = {
+                "image": ("emoji.gif", gif.content, "image/gif")
+            }
+
+            data = {
+                "token": os.environ["USER_SLACK_TOKEN"],
+                "name": new_name,
+                "mode": "url",
+                "url": gif_url,
+                "search_args": "{}",
+                "_x_reason": "add-custom-emoji-dialog-content",
+                "_x_mode": "online",
+                "_x_sonic": "true",
+                "_x_app_name": "client",
+            }
+
+            headers = {
+                "accept": "*/*",
+                "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+                "priority": "u=1, i",
+                "sec-ch-ua": '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"macOS"',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site",
+            }
+
+            cookies = {
+                "utm": "{}",
+                "b": os.environ["USER_COOKIE_B"],
+                "tz": "330",
+                "c": '{"banner_homepage_slackbot":1}',
+                "d-s": "1773046862",
+                "x": os.environ["USER_COOKIE_X"],
+                "d": os.environ["USER_COOKIE_D"],
+            }
+
+            emojir = requests.post(f"https://{workspaceid}.slack.com/api/emoji.add" + os.environ["URL_PARAMS"], headers=headers, cookies=cookies, data=data)
+
+            emojir_json = emojir.json()
+            print(emojir_json)
         
         elif ping_match and not files:
             
@@ -153,9 +235,9 @@ def message(payload):
                 emojir_json = emojir.json()
                 print(emojir_json)
 
-                client.chat_postMessage(channel=channel_id, thread_ts=ts, text=f"emoji added :{emoji_name}:")
+                client.chat_postMessage(channel=channel_id, thread_ts=ts, text=f"emoji added :{new_name}:")
                 client.reactions_remove(channel=channel_id, name='loading', timestamp=ts)
-                client.reactions_add(channel=channel_id, name=emoji_name, timestamp=ts)
+                client.reactions_add(channel=channel_id, name=new_name, timestamp=ts)
 
         elif files and text: 
             file = files[0]
